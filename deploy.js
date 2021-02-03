7804ecd77cc9b260bcfd3f5829379afb7f328e22
Vytenis Urbonavičius <vytenisu@wix.com>
2021-02-03 14:25:56 +0200

    Added download and remove functionality, made file name custom, sorted out prettier, removed unneeded files

--- deploy.js
+++ deploy.js
@@ -1,19 +1,21 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
-'use strict';
+"use strict";
 
-const { spawnSync } = require('child_process');
+const { spawnSync } = require("child_process");
 const fs = require("fs");
 
 // Parameters
-let region = 'us-east-1';
+let region = "us-east-1";
 let imageId = ``;
 let bucket = ``;
 let stack = ``;
 let ecrDockerImageArn = ``;
 
 function usage() {
-  console.log(`Usage: deploy.sh [-r region] [-b bucket] [-s stack] [-i docker-image]`);
+  console.log(
+    `Usage: deploy.sh [-r region] [-b bucket] [-s stack] [-i docker-image]`
+  );
   console.log(`  -r, --region       Target region, default '${region}'`);
   console.log(`  -b, --s3-bucket    S3 bucket for deployment, required`);
   console.log(`  -s, --stack-name   CloudFormation stack name, required`);
@@ -22,10 +24,23 @@ function usage() {
 }
 
 function ensureBucket() {
-  const s3Api = spawnSync('aws', ['s3api', 'head-bucket', '--bucket', `${bucket}`, '--region', `${region}`]);
+  const s3Api = spawnSync("aws", [
+    "s3api",
+    "head-bucket",
+    "--bucket",
+    `${bucket}`,
+    "--region",
+    `${region}`,
+  ]);
   if (s3Api.status !== 0) {
     console.log(`Creating S3 bucket ${bucket}`);
-    const s3 = spawnSync('aws', ['s3', 'mb', `s3://${bucket}`, '--region', `${region}`]);
+    const s3 = spawnSync("aws", [
+      "s3",
+      "mb",
+      `s3://${bucket}`,
+      "--region",
+      `${region}`,
+    ]);
     if (s3.status !== 0) {
       console.log(`Failed to create bucket: ${JSON.stringify(s3)}`);
       console.log((s3.stderr || s3.stdout).toString());
@@ -37,20 +52,26 @@ function ensureBucket() {
 function ensureEC2ImageId() {
   // Fetching the ECS optimized AMI for AL2
   // More info: https://aws.amazon.com/premiumsupport/knowledge-center/launch-ecs-optimized-ami/
-  imageId = spawnSync('aws', ['ssm', 'get-parameters', 
-                              '--names', '/aws/service/ecs/optimized-ami/amazon-linux-2/recommended/image_id', 
-                              '--region', `${region}`, 
-                              '--query', '"Parameters[0].Value"']);
-  if(!imageId.length) {
+  imageId = spawnSync("aws", [
+    "ssm",
+    "get-parameters",
+    "--names",
+    "/aws/service/ecs/optimized-ami/amazon-linux-2/recommended/image_id",
+    "--region",
+    `${region}`,
+    "--query",
+    '"Parameters[0].Value"',
+  ]);
+  if (!imageId.length) {
     // Setting image ID optimized for us-east-1
     // Mode info: https://docs.aws.amazon.com/AmazonECS/latest/developerguide/ecs-optimized_AMI.html
-    imageId = 'ami-00f69adbdc780866c'; 
+    imageId = "ami-00f69adbdc780866c";
   }
 }
 
 function getArgOrExit(i, args) {
   if (i >= args.length) {
-    console.log('Too few arguments');
+    console.log("Too few arguments");
     usage();
     process.exit(1);
   }
@@ -61,21 +82,26 @@ function parseArgs() {
   var args = process.argv.slice(2);
   var i = 0;
   while (i < args.length) {
-    switch(args[i]) {
-      case '-h': case '--help':
+    switch (args[i]) {
+      case "-h":
+      case "--help":
         usage();
         process.exit(0);
         break;
-      case '-r': case '--region':
+      case "-r":
+      case "--region":
         region = getArgOrExit(++i, args);
         break;
-      case '-b': case '--s3-bucket':
+      case "-b":
+      case "--s3-bucket":
         bucket = getArgOrExit(++i, args);
         break;
-      case '-s': case '--stack-name':
+      case "-s":
+      case "--stack-name":
         stack = getArgOrExit(++i, args);
         break;
-      case '-i': case '--docker-image':
+      case "-i":
+      case "--docker-image":
         ecrDockerImageArn = getArgOrExit(++i, args);
         break;
       default:
@@ -86,7 +112,7 @@ function parseArgs() {
     ++i;
   }
   if (!stack.trim() || !bucket.trim() || !ecrDockerImageArn.trim()) {
-    console.log('Missing required parameters');
+    console.log("Missing required parameters");
     usage();
     process.exit(1);
   }
@@ -98,10 +124,12 @@ function spawnOrFail(command, args, options) {
     console.log(`Command ${command} failed with ${cmd.error.code}`);
     process.exit(255);
   }
-  const output=cmd.stdout.toString();
-  
+  const output = cmd.stdout.toString();
+
   if (cmd.status !== 0) {
-    console.log(`Command ${command} failed with exit code ${cmd.status} signal ${cmd.signal}`);
+    console.log(
+      `Command ${command} failed with exit code ${cmd.status} signal ${cmd.signal}`
+    );
     console.log(cmd.stderr.toString());
     process.exit(cmd.status);
   }
@@ -109,60 +137,131 @@ function spawnOrFail(command, args, options) {
 }
 
 function ensureTools() {
-  spawnOrFail('aws', ['--version']);
-  spawnOrFail('sam', ['--version']);
+  spawnOrFail("aws", ["--version"]);
+  spawnOrFail("sam", ["--version"]);
 }
 
 parseArgs();
 ensureTools();
 
-if (!fs.existsSync('build')) {
-  fs.mkdirSync('build');
+if (!fs.existsSync("build")) {
+  fs.mkdirSync("build");
 }
 
 console.log(`Using region ${region}, bucket ${bucket}, stack ${stack}`);
 ensureEC2ImageId();
 ensureBucket();
 
-spawnOrFail('sam', ['package', '--s3-bucket', `${bucket}`,
-                    '--template-file', 'templates/RecordingDemoCloudformationTemplate.yaml',
-                    '--output-template-file', 'build/packaged.yaml',
-                    '--region',  `${region}`]);
+spawnOrFail("sam", [
+  "package",
+  "--s3-bucket",
+  `${bucket}`,
+  "--template-file",
+  "templates/RecordingCloudformationTemplate.yaml",
+  "--output-template-file",
+  "build/packaged.yaml",
+  "--region",
+  `${region}`,
+]);
 
-console.log('Deploying recording application');
-const output=spawnOrFail('sam', ['deploy', '--template-file', './build/packaged.yaml', '--stack-name', `${stack}`,
-                    '--parameter-overrides', `ECRDockerImageArn=${ecrDockerImageArn}`,
-                    '--capabilities', 'CAPABILITY_IAM', '--region', `${region}`, '--no-fail-on-empty-changeset']);
+console.log("Deploying recording application");
+const output = spawnOrFail("sam", [
+  "deploy",
+  "--template-file",
+  "./build/packaged.yaml",
+  "--stack-name",
+  `${stack}`,
+  "--parameter-overrides",
+  `ECRDockerImageArn=${ecrDockerImageArn}`,
+  "--capabilities",
+  "CAPABILITY_IAM",
+  "--region",
+  `${region}`,
+  "--no-fail-on-empty-changeset",
+]);
 console.log(output);
 
-const invokeUrl=spawnOrFail('aws', ['cloudformation', 'describe-stacks', '--stack-name', `${stack}`,
-                    '--query', 'Stacks[0].Outputs[0].OutputValue', '--output', 'text', '--region', `${region}`]);
+const invokeUrl = spawnOrFail("aws", [
+  "cloudformation",
+  "describe-stacks",
+  "--stack-name",
+  `${stack}`,
+  "--query",
+  "Stacks[0].Outputs[0].OutputValue",
+  "--output",
+  "text",
+  "--region",
+  `${region}`,
+]);
 console.log(`Recording API Gateway invoke URL: ${invokeUrl}`);
 
-const ecsClusterName=spawnOrFail('aws', ['cloudformation', 'describe-stacks', '--stack-name', `${stack}`,
-                    '--query', 'Stacks[0].Outputs[1].OutputValue', '--output', 'text', '--region', `${region}`]).trim();
-console.log('Adding ECS capacity provider and enabling managed scaling & termination protection');
+const ecsClusterName = spawnOrFail("aws", [
+  "cloudformation",
+  "describe-stacks",
+  "--stack-name",
+  `${stack}`,
+  "--query",
+  "Stacks[0].Outputs[1].OutputValue",
+  "--output",
+  "text",
+  "--region",
+  `${region}`,
+]).trim();
+console.log(
+  "Adding ECS capacity provider and enabling managed scaling & termination protection"
+);
 
-const autoScalingGroupName=spawnOrFail('aws', ['cloudformation', 'describe-stacks', '--stack-name', `${stack}`,
-                    '--query', 'Stacks[0].Outputs[2].OutputValue', '--output', 'text', '--region', `${region}`]).trim();
-
+const autoScalingGroupName = spawnOrFail("aws", [
+  "cloudformation",
+  "describe-stacks",
+  "--stack-name",
+  `${stack}`,
+  "--query",
+  "Stacks[0].Outputs[2].OutputValue",
+  "--output",
+  "text",
+  "--region",
+  `${region}`,
+]).trim();
 
 // Enabling instance termination perotection from scale-in
-spawnSync('aws', ['autoscaling', 'update-auto-scaling-group', 
-                    '--auto-scaling-group-name', `${autoScalingGroupName}`, '--new-instances-protected-from-scale-in']);
+spawnSync("aws", [
+  "autoscaling",
+  "update-auto-scaling-group",
+  "--auto-scaling-group-name",
+  `${autoScalingGroupName}`,
+  "--new-instances-protected-from-scale-in",
+]);
 
-const asg = JSON.parse(spawnOrFail('aws', ['autoscaling', 'describe-auto-scaling-groups', '--auto-scaling-group-name', `${autoScalingGroupName}`]));
-const autoScalingGroupInstances = asg.AutoScalingGroups && asg.AutoScalingGroups[0].Instances;
-const autoScalingGroupArn = asg.AutoScalingGroups && asg.AutoScalingGroups[0].AutoScalingGroupARN;
-const autoScalingGroupCapacityProviderName = autoScalingGroupName + 'CapacityProvider';
+const asg = JSON.parse(
+  spawnOrFail("aws", [
+    "autoscaling",
+    "describe-auto-scaling-groups",
+    "--auto-scaling-group-name",
+    `${autoScalingGroupName}`,
+  ])
+);
+const autoScalingGroupInstances =
+  asg.AutoScalingGroups && asg.AutoScalingGroups[0].Instances;
+const autoScalingGroupArn =
+  asg.AutoScalingGroups && asg.AutoScalingGroups[0].AutoScalingGroupARN;
+const autoScalingGroupCapacityProviderName =
+  autoScalingGroupName + "CapacityProvider";
 var instanceIds = [];
-autoScalingGroupInstances.forEach(instance => {
+autoScalingGroupInstances.forEach((instance) => {
   instanceIds.push(instance.InstanceId);
 });
 
-spawnOrFail('aws', ['autoscaling', 'set-instance-protection', 
-                    '--auto-scaling-group-name', `${autoScalingGroupName}`, 
-                    '--protected-from-scale-in', '--instance-ids', `${instanceIds[0]}`, `${instanceIds[1]}`]);
+spawnOrFail("aws", [
+  "autoscaling",
+  "set-instance-protection",
+  "--auto-scaling-group-name",
+  `${autoScalingGroupName}`,
+  "--protected-from-scale-in",
+  "--instance-ids",
+  `${instanceIds[0]}`,
+  `${instanceIds[1]}`,
+]);
 
 // Create a capacity provider with managed scale-in
 let capacityProviderParam = {
@@ -171,18 +270,30 @@ let capacityProviderParam = {
     status: "ENABLED",
     targetCapacity: 60,
     minimumScalingStepSize: 1,
-    maximumScalingStepSize: 1
+    maximumScalingStepSize: 1,
   },
-  managedTerminationProtection: "ENABLED"
-}
-spawnOrFail('aws', ['ecs', 'create-capacity-provider', 
-                    '--name', `${autoScalingGroupCapacityProviderName}`, '--auto-scaling-group-provider', 
-                    `${JSON.stringify(capacityProviderParam)}`]);
+  managedTerminationProtection: "ENABLED",
+};
+spawnOrFail("aws", [
+  "ecs",
+  "create-capacity-provider",
+  "--name",
+  `${autoScalingGroupCapacityProviderName}`,
+  "--auto-scaling-group-provider",
+  `${JSON.stringify(capacityProviderParam)}`,
+]);
 let defaultCapacityProvider = {
-    capacityProvider: autoScalingGroupCapacityProviderName
-  }
+  capacityProvider: autoScalingGroupCapacityProviderName,
+};
 
-spawnOrFail('aws', ['ecs', 'put-cluster-capacity-providers', 
-                    '--cluster', `${ecsClusterName}`, '--capacity-providers', `${autoScalingGroupCapacityProviderName}`, 
-                    '--default-capacity-provider-strategy', `${JSON.stringify(defaultCapacityProvider)}`]);
-console.log('Deployment complete')
+spawnOrFail("aws", [
+  "ecs",
+  "put-cluster-capacity-providers",
+  "--cluster",
+  `${ecsClusterName}`,
+  "--capacity-providers",
+  `${autoScalingGroupCapacityProviderName}`,
+  "--default-capacity-provider-strategy",
+  `${JSON.stringify(defaultCapacityProvider)}`,
+]);
+console.log("Deployment complete");
--- src/index.js
+++ src/index.js
@@ -1,156 +1,259 @@
 // Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
 // SPDX-License-Identifier: Apache-2.0
 
-'use strict';
+"use strict";
 
-var AWS = require('aws-sdk');
+var AWS = require("aws-sdk");
+const { S3Utils } = require("../recording/utils/s3");
 var ecs = new AWS.ECS();
 
 // Reading environment variables
 const ecsClusterArn = process.env.ecsClusterArn;
-const ecsTaskDefinationArn = process.env.ecsTaskDefinationArn;
+const ecsTaskDefinitionArn = process.env.ecsTaskDefinitionArn;
 const ecsContainerName = process.env.ecsContainerName;
 const recordingArtifactsBucket = process.env.recordingArtifactsBucket;
 
 let responseBody = {
-    message: '',
-    input: ''
+  message: "",
+  input: "",
 };
 
 let response = {
-    statusCode: 200,
-    headers: {},
-    body: ''
-};
- 
-exports.handler = function(event, context, callback) {
-    let meetingURL = "";
-    let taskId = "";
-    let recordingAction = "";
-    
-    console.log(event);
-    responseBody.input = event;
-    
-    if(event.queryStringParameters && event.queryStringParameters.recordingAction) {
-        console.log("Recording action: " + event.queryStringParameters.recordingAction);
-        recordingAction = event.queryStringParameters.recordingAction;
-    }
-    
-    switch(recordingAction.toLowerCase()) {
-        case 'start':
-            if(event.queryStringParameters && event.queryStringParameters.meetingURL) {
-                console.log("Meeting URL: " + event.queryStringParameters.meetingURL);
-                meetingURL = decodeURIComponent(event.queryStringParameters.meetingURL);
-                return startRecording(event, context, callback, meetingURL);
-            } else {
-                responseBody = {
-                    message: "Missing parameter: meetingURL",
-                    input: event
-                };
-                response = {
-                    statusCode: 400,
-                    headers: {},
-                    body: JSON.stringify(responseBody, null, ' ')
-                };
-                context.succeed(response);
-            }
-        case 'stop':
-            if(event.queryStringParameters && event.queryStringParameters.taskId) {
-                console.log("ECS task ID: " + event.queryStringParameters.taskId);
-                taskId = event.queryStringParameters.taskId;
-                return stopRecording(event, context, taskId);
-            } else {
-                responseBody = {
-                    message: "Missing parameter: taskId",
-                    input: event
-                };
-                response = {
-                    statusCode: 400,
-                    headers: {},
-                    body: JSON.stringify(responseBody, null, ' ')
-                };
-                context.succeed(response);
-            }
-        default:
-            responseBody = {
-                message: "Invalid parameter: recordingAction. Valid values 'start' & 'stop'",
-                input: event
-            };
-            response = {
-                statusCode: 400,
-                headers: {},
-                body: JSON.stringify(responseBody)
-            };
-    }
-    
-    console.log("response: " + JSON.stringify(response));
-    callback(null, response);
+  statusCode: 200,
+  headers: {},
+  body: "",
 };
 
-function startRecording(event, context, callback, meetingUrl) {
-    let ecsRunTaskParams = {
-        cluster: ecsClusterArn,
-        launchType: "EC2",
-        count: 1,
-        overrides: {
-            containerOverrides: [ 
-                 { 
-                    environment: [ 
-                        { 
-                            name: "MEETING_URL",
-                            value: meetingUrl
-                        },
-                        {
-                            name: "RECORDING_ARTIFACTS_BUCKET",
-                            value: recordingArtifactsBucket
-                        }
-                    ],
-                    name: ecsContainerName
-                }
-            ],
+exports.handler = function (event, context, callback) {
+  let targetURL = "";
+  let recordingName = "";
+  let taskId = "";
+  let action = "";
+
+  console.log(event);
+  responseBody.input = event;
+
+  if (event.queryStringParameters && event.queryStringParameters.action) {
+    console.log("Recording action: " + event.queryStringParameters.action);
+    action = event.queryStringParameters.action;
+  }
+
+  let ensureParameterExists = (parameterName) => {
+    if (
+      !event.queryStringParameters ||
+      !event.queryStringParameters[parameterName]
+    ) {
+      responseBody = {
+        message: `Missing parameter: ${parameterName}`,
+        input: event,
+      };
+      response = {
+        statusCode: 400,
+        headers: {},
+        body: JSON.stringify(responseBody, null, " "),
+      };
+      context.succeed(response);
+      return false;
+    }
+
+    return true;
+  };
+
+  switch (action.toLowerCase()) {
+    case "start":
+      if (
+        !ensureParameterExists("targetURL") ||
+        !ensureParameterExists("recordingName")
+      ) {
+        break;
+      }
+
+      console.log("Target URL: " + event.queryStringParameters.targetURL);
+      console.log(
+        "Recording file name: " + event.queryStringParameters.recordingName
+      );
+      targetURL = decodeURIComponent(event.queryStringParameters.targetURL);
+      recordingName = decodeURIComponent(
+        event.queryStringParameters.recordingName
+      );
+
+      return startRecording(event, context, callback, targetURL, recordingName);
+
+    case "stop":
+      if (!ensureParameterExists("taskId")) {
+        break;
+      }
+
+      console.log("ECS task ID: " + event.queryStringParameters.taskId);
+      taskId = event.queryStringParameters.taskId;
+      return stopRecording(event, context, taskId);
+
+    case "download":
+      if (!ensureParameterExists("recordingName")) {
+        break;
+      }
+
+      console.log(
+        "Recording file name: " + event.queryStringParameters.recordingName
+      );
+
+      recordingName = decodeURIComponent(
+        event.queryStringParameters.recordingName
+      );
+
+      const s3 = new S3Utils(recordingArtifactsBucket, recordingName);
+
+      s3.read.then((recording) => {
+        response = {
+          statusCode: 200,
+          headers: {
+            "Content-Type": "video/mp4",
+            "Content-Disposition": `attachment; filename="${recordingName}.mp4"`,
+            "Content-Length": Buffer.byteLength(recording),
+          },
+          body: recording,
+        };
+
+        context.succeed(response);
+      });
+
+      break;
+
+    case "delete":
+      if (!ensureParameterExists("recordingName")) {
+        break;
+      }
+
+      console.log(
+        "Recording file name: " + event.queryStringParameters.recordingName
+      );
+
+      recordingName = decodeURIComponent(
+        event.queryStringParameters.recordingName
+      );
+
+      const s3 = new S3Utils(recordingArtifactsBucket, recordingName);
+
+      s3.remove()
+        .then(() => {
+          responseBody = {
+            message: `OK`,
+          };
+
+          response = {
+            statusCode: 200,
+            headers: {},
+            body: JSON.stringify(responseBody, null, " "),
+          };
+
+          context.succeed(response);
+        })
+        .catch(() => {
+          responseBody = {
+            message: `Could not delete recording: ${recordingName}`,
+            input: event,
+          };
+          response = {
+            statusCode: 500,
+            headers: {},
+            body: JSON.stringify(responseBody, null, " "),
+          };
+          context.succeed(response);
+        });
+
+      break;
+
+    default:
+      responseBody = {
+        message:
+          "Invalid parameter: recordingAction. Valid values 'start', 'stop', 'download', 'delete'",
+        input: event,
+      };
+      response = {
+        statusCode: 400,
+        headers: {},
+        body: JSON.stringify(responseBody),
+      };
+  }
+
+  console.log("response: " + JSON.stringify(response));
+  callback(null, response);
+};
+
+function startRecording(event, context, callback, targetURL, recordingName) {
+  let ecsRunTaskParams = {
+    cluster: ecsClusterArn,
+    launchType: "EC2",
+    count: 1,
+    overrides: {
+      containerOverrides: [
+        {
+          environment: [
+            {
+              name: "TARGET_URL",
+              value: targetURL,
+            },
+            {
+              name: "OUTPUT_FILE_NAME",
+              value: recordingName,
+            },
+            {
+              name: "RECORDING_ARTIFACTS_BUCKET",
+              value: recordingArtifactsBucket,
+            },
+          ],
+          name: ecsContainerName,
         },
-        placementConstraints: [{
-            type: "distinctInstance"
-        }],
-        taskDefinition: ecsTaskDefinationArn
-    };
-    
-    ecs.runTask(ecsRunTaskParams, function(err, data) {
-        if (err) {
-            console.log(err);   // an error occurred
-            response.statusCode = err.statusCode;
-            response.body = JSON.stringify(err, null, ' ');
-            context.succeed(response);
-        }
-        else {
-            console.log(data);  // successful response
-            response.statusCode = 200;
-            response.body = JSON.stringify((data.tasks.length && data.tasks[0].taskArn) ? data.tasks[0].taskArn : data, null, ' ');
-            context.succeed(response);
-        }
-    });
+      ],
+    },
+    placementConstraints: [
+      {
+        type: "distinctInstance",
+      },
+    ],
+    taskDefinition: ecsTaskDefinitionArn,
+  };
+
+  ecs.runTask(ecsRunTaskParams, function (err, data) {
+    if (err) {
+      console.log(err); // an error occurred
+      response.statusCode = err.statusCode;
+      response.body = JSON.stringify(err, null, " ");
+      context.succeed(response);
+    } else {
+      console.log(data); // successful response
+      response.statusCode = 200;
+      response.body = JSON.stringify(
+        data.tasks.length && data.tasks[0].taskArn
+          ? data.tasks[0].taskArn
+          : data,
+        null,
+        " "
+      );
+      context.succeed(response);
+    }
+  });
 }
 
 function stopRecording(event, context, taskId) {
-    let ecsStopTaskParam = {
-        cluster: ecsClusterArn,
-        task: taskId
-    };
-    
-    ecs.stopTask(ecsStopTaskParam, function(err, data) {
-        if (err) {
-            console.log(err);   // an error occurred
-            response.statusCode = err.statusCode;
-            response.body = JSON.stringify(err, null, ' ');
-            context.succeed(response);
-        }
-        else {
-            console.log(data);  // successful response
-            response.statusCode = 200;
-            responseBody = data;
-            response.body = JSON.stringify(data, null, ' ');
-            console.log("Stop task succeeded.", response);
-            context.succeed(response);
-        }
-    });
+  let ecsStopTaskParam = {
+    cluster: ecsClusterArn,
+    task: taskId,
+  };
+
+  ecs.stopTask(ecsStopTaskParam, function (err, data) {
+    if (err) {
+      console.log(err); // an error occurred
+      response.statusCode = err.statusCode;
+      response.body = JSON.stringify(err, null, " ");
+      context.succeed(response);
+    } else {
+      console.log(data); // successful response
+      response.statusCode = 200;
+      responseBody = data;
+      response.body = JSON.stringify(data, null, " ");
+      console.log("Stop task succeeded.", response);
+      context.succeed(response);
+    }
+  });
 }
